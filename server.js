b65b21f2824b818d5c518043bea408103b4efcaf
chrisso emma <chrissoemma@gmail.com>
2019-05-10 09:56:05 +0300

    public files added

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const app =express();
 const port=process.env.PORT || 3000;
 const server=app.listen(port);
 
+ //public files setup
+app.use(express.static('public'));
+
+ 
 //socket setup
 const socket=require('socket.io');
 const io=socket(server);
